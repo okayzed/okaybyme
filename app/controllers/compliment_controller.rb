83e934f36796687dc2aab7d23b31c0aaaecc88eb
natan <nnzohar@CaeN.(none)>
2009-08-08 23:56:29 -0700

    ajaxify me. please.

--- app/controllers/compliment_controller.rb
+++ app/controllers/compliment_controller.rb
@@ -4,4 +4,18 @@ class ComplimentController < ApplicationController
     images = Dir::glob "public/images/backgrounds/**/*.png"
     @background_image = images.rand.gsub!(/public\/images\//, "")
   end
+
+  def ajax_compliment
+    compliment = ComplimentGenerator.full_compliment.downcase
+    render :text => compliment
+  end
+
+  def ajax_why
+    why_text =
+      """
+      insincere compliments from sincere robots. but... why? because i need
+      cheering up.
+      """
+    render :text => why_text
+  end
 end
